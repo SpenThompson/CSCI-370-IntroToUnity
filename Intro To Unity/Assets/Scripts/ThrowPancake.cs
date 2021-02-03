6888f7ee87dab6e9fc8ea268896322d5acdc7d4c
Little_Snake <dmitryvy@ut.ee>
2021-02-03 09:47:31 +0200

    Pancakes thrown and mouse follows

---
+++ Intro To Unity/Assets/Scripts/ThrowPancake.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowPancake : MonoBehaviour
+{
+    public GameObject pancake;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("DropPancake2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DropPancake()
+    {
+        Instantiate(pancake);
+    }
+
+    IEnumerator DropPancake2()
+    {
+        yield return new WaitForSeconds(2f);
+        for (; ; )
+        {
+            Instantiate(pancake);
+            yield return new WaitForSeconds(Random.Range(2f, 4f));
+        }
+    }
+}
