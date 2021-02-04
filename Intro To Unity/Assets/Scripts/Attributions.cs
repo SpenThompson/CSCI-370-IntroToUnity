a2c366bbeb66ef6e356e47eda6323290f23f8ece
Spencer <james.spencer.thompson@gmail.com>
2021-02-04 10:38:57 -0600

    Attributions Added

---
+++ Intro To Unity/Assets/Scripts/Attributions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attributions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* Sound Attributions
+           Bird Caw: https://freesound.org/people/vixuxx/sounds/9874/
+           Falling: https://freesound.org/people/plasterbrain/sounds/395443/
+           Splat: https://freesound.org/people/moshang/sounds/460869/
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
